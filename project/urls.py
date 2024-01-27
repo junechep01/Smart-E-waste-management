c523c2e6020c9055d0a86c0f7246bd74dc3bde25
June Chepngetich <junechep2002@gmail.com>
2024-01-27 18:13:05 +0300

    initial

---
+++ project/urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path
+from . import views
+from .views import PostListView,PostDetailView,PostCreateView,PostUpdateView,PostDeleteView
+
+urlpatterns = [
+    path('', views.home, name='project-home'),
+    path('about/', views.about, name='project-about'),
+    path('contacts/', views.contacts, name='project-contacts'),
+    # path('recycling/', views.recycling, name='project-recycling'),
+    path('talk/', PostListView.as_view(), name='project-talk'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+]
---
+++ project/views.py
@@ -0,0 +1,81 @@
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView,DetailView,CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from project.models import Post
+
+
+
+
+def home(request):
+	return render(request, 'project/home.html')
+
+
+def about(request):
+	return render(request, 'project/about.html', {'title':'About'})
+
+def profile(request):
+
+	context={
+		'users':'users'
+	}
+	return render(request, 'project/profile.html', context, {'title':'Profile'})
+	
+
+@login_required
+def contacts(request):
+	return render(request, 'project/contacts.html', {'title':'Contacts'})
+
+
+
+@login_required
+def talk(request):
+	context = {
+		'posts': Post.objects.all()}
+	return render (request,'project/talk.html', context)
+
+class PostListView(ListView):
+	model=Post
+	template_name='project/talk.html'
+	context_object_name='posts'
+	ordering=['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model=Post
+ 
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = 'talk/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
